5ea238076a1a9b9c3b10f4c804a2e5bc68cfc001
Adam Walker <adamwalker567123@gmail.com>
2020-01-04 00:01:40 -0800

    remove groupifier-next extension

--- src/logic/auth.js
+++ src/logic/auth.js
@@ -63,7 +63,7 @@ export const signIn = () => {
   const params = new URLSearchParams({
     client_id: WCA_OAUTH_CLIENT_ID,
     response_type: 'token',
-    redirect_uri: oauthRedirectUri() + '/groupifier-next',
+    redirect_uri: oauthRedirectUri(),
     scope: 'manage_competitions',
   });
   window.location = `${WCA_ORIGIN}/oauth/authorize?${params.toString()}`;
